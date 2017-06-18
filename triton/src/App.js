fc875c85f65c3c48aa9c03884fd43e488ec599f8
jpenson <jpenson@uea.ac.uk>
2017-06-18 14:17:54 +0100

    updated appbar with new theme and stylings

--- triton/src/App.js
+++ triton/src/App.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {green500, green700, grey400, deepPurple900, deepPurpleA700, deepPurpleA100} from 'material-ui/styles/colors';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
 import './App.css';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: green500,
+    primary2Color: green700,
+    primary3Color: grey400,
+    accent1Color: deepPurpleA700,
+    accent2Color: deepPurpleA100,
+    accent3Color: deepPurple900,
+  }
+});
+
+const styles = {
+  appBar: {
+   flexWrap: 'wrap',
+  },
+  tabs: {
+   width: '50%',
+   paddingTop: '0.5ex',
+   paddingRight: '20%'
+  },
+};
+
+const Navigation = () => (
+  <Tabs style={styles.tabs}>
+    <Tab label="About" />
+    <Tab label="Graphs" />
+    <Tab label="Resources"/>
+  </Tabs>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Triton</h2>
-        </div>
-        <p className="App-intro">
-          Climate Data plotting tool created by Jack Penson
-        </p>
-      </div>
+     <MuiThemeProvider muiTheme={muiTheme}>
+        <AppBar title="TRITON" style={styles.appBar} showMenuIconButton={false}>
+          <Navigation />
+        </AppBar>
+     </MuiThemeProvider>
     );
   }
 }
